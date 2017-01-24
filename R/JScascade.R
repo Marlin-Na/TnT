94979ec4ef8b5c731dff9de34a22dc6bd7f975a8
Marlin- <marlin-@gmx.cn>
2017-01-25 06:10:36 +0800

    Adjust file structure

--- R/JScascade.R
+++ R/JScascade.R
@@ -1,15 +1,3 @@
-#' @importFrom methods is
-NULL
-
-#' @importFrom htmlwidgets JS
-#' @export
-htmlwidgets::JS
-
-#' @export
-print.JS_EVAL <- function (x, ...) {
-    cat(x)
-    invisible(x)
-}
 
 
 #' Construct a Javascript Function Cascade
---
+++ R/misc.R
@@ -0,0 +1,19 @@
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+#' @importFrom methods is
+NULL
+
+#' @importFrom htmlwidgets JS
+#' @export
+htmlwidgets::JS
+
+#' @export
+print.JS_EVAL <- function (x, ...) {
+    cat(x)
+    invisible(x)
+}
+
+
