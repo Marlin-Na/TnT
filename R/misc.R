1272e6069bb07f129fb2386c7ab39714e3521ceb
Marlin- <marlin-@gmx.cn>
2017-02-16 23:27:55 +0800

    Print `JS_EVAL` with indentation

--- R/misc.R
+++ R/misc.R
@@ -12,7 +12,19 @@ htmlwidgets::JS
 
 #' @export
 print.JS_EVAL <- function (x, ...) {
-    cat(x)
+    #cat(x)
+    #invisible(x)
+    
+    # Print with indentation
+    byline <- strsplit(x, "\n", fixed = TRUE)[[1]]
+    l_par_cum <- cumsum(lengths(regmatches(byline, gregexpr("(", byline, fixed = TRUE))))
+    r_par_cum <- cumsum(lengths(regmatches(byline, gregexpr(")", byline, fixed = TRUE))))
+    indent <- l_par_cum - r_par_cum
+    spaces <- sapply(indent, FUN = function (n) {
+        paste(rep("  ", n), collapse = "")
+    })
+    out <- paste0(byline, "\n", spaces)
+    cat(out)
     invisible(x)
 }
 
