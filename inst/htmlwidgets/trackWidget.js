8e3479ee11d2976ca4bdc6c6c144d63b9bb1084c
Marlin- <marlin-@gmx.cn>
2017-06-23 20:00:22 +0800

    Generators of track data retriever

--- inst/htmlwidgets/trackWidget.js
+++ inst/htmlwidgets/trackWidget.js
@@ -5,6 +5,44 @@ HTMLWidgets.widget({
     type: 'output',
 
     factory: function(el, width, height) {
+        
+        // Some util functions
+        var tnr = {};
+        
+        tnr.range_data_retriever = function (data) {
+            var rangeData = data;
+            // Return a closure as the data retriever
+            var ans = function (loc) {
+                var min = loc.from - 100;
+                var max = loc.to  + 100;
+                var ans = [];
+                for (var i = 0; i < data.length; i++) {
+                    var row = rangeData[i];
+                    if (row.start <= max && row.end >= min) {
+                        ans.push(row);
+                    }
+                }
+                return ans;
+            };
+            return ans;
+        };
+        tnr.pos_data_retriever = function (data) {
+            var posData = data;
+            var ans = function (loc) {
+                var min = loc.from - 100;
+                var max = loc.to + 100;
+                var ans = [];
+                for (var i = 0; i < data.length; i++) {
+                    var row = posData[i];
+                    if (row.start <= max && row.end >= min) {
+                        ans.push(row);
+                    }
+                }
+                return ans;
+            };
+            return ans;
+        };
+        
 
         // Define shared variables for this instance
         var tntins = null;
