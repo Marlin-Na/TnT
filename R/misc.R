dc0d43528071438c32cf9df23286643c146d4858
Marlin- <marlin-@gmx.cn>
2017-07-05 10:10:25 +0800

    Move util functions to misc.R

--- R/misc.R
+++ R/misc.R
@@ -29,3 +29,54 @@ ul <- function(x)
 `ul<-` <- function (x, value)
     relist(value, x)
 
+
+#' @export
+splitdf <- function (df, f) {
+    # We need to speed up this function
+    if (requireNamespace("data.table", quietly = TRUE)) {
+        # relative faster
+        df[["___FACTOR___"]] <- f
+        ldf <- data.table:::split.data.table(
+            data.table::as.data.table(df),
+            by = "___FACTOR___", keep.by = FALSE, flatten = FALSE
+        )
+    }
+    else {
+        warning("Install data.table could speed up the spliting process")
+        ldf <- split(df, f)
+    }
+    ldf
+}
+
+
+#' @export
+strandlabel <- function (labels, strands) {
+    if (length(labels))
+        ifelse(strands == "+", paste(labels, ">"),
+            ifelse(strands == "-", paste("<", labels), labels))
+    else
+        ifelse(strands == "+", ">",
+            ifelse(strands == "-", "<", ""))
+}
+
+#' @export
+.removeAsIs <- function (df) {
+    # The nested data frame converted from GRanges/DataFrame will have a
+    # "AsIs" class, which will cause the data frame can not be shown and
+    # can not be converted to JSON correctly.
+    for (i in seq_along(df)) {
+        element <- df[[i]]
+        if (is.data.frame(element)) {
+            class(element) <- class(element)[class(element) != "AsIs"]
+            df[[i]] <- .removeAsIs(element)
+        }
+    }
+    df
+}
+
+#' @export
+.df2json <- function (df) {
+    df <- .removeAsIs(df)
+    jsonlite::toJSON(df, dataframe = "rows", pretty = 2)
+}
+
--- R/tntTracks.R
+++ R/tntTracks.R
@@ -178,15 +178,6 @@ PosTrackData <- function (pos, tooltip = mcols(pos)) {
     trackdata
 }
 
-#' @export
-strandlabel <- function (labels, strands) {
-    if (length(labels))
-        ifelse(strands == "+", paste(labels, ">"),
-            ifelse(strands == "-", paste("<", labels), labels))
-    else
-        ifelse(strands == "+", ">",
-            ifelse(strands == "-", "<", ""))
-}
 
 #' @export
 GeneTrackData <- function (range, labels = paste("Gene", mcols(range)$gene_id),
@@ -286,24 +277,6 @@ TxTrackData <- function () {
     
 }
 
-#' @export
-splitdf <- function (df, f) {
-    # We need to speed up this function
-    if (requireNamespace("data.table", quietly = TRUE)) {
-        # relative faster
-        df[["___FACTOR___"]] <- f
-        ldf <- data.table:::split.data.table(
-            data.table::as.data.table(df),
-            by = "___FACTOR___", keep.by = FALSE, flatten = FALSE
-        )
-    }
-    else {
-        warning("Install data.table could speed up the spliting process")
-        ldf <- split(df, f)
-    }
-    ldf
-}
-
 
 #' @export
 TxTrackDataFromTxDb <- function (txdb, seqlevel = seqlevels(txdb)) {
@@ -385,26 +358,6 @@ setValidity("PosTrackData",
 setGeneric("compileTrackData",
            function (trackData) standardGeneric("compileTrackData"))
 
-#' @export
-.removeAsIs <- function (df) {
-    # The nested data frame converted from GRanges/DataFrame will have a
-    # "AsIs" class, which will cause the data frame can not be shown and
-    # can not be converted to JSON correctly.
-    for (i in seq_along(df)) {
-        element <- df[[i]]
-        if (is.data.frame(element)) {
-            class(element) <- class(element)[class(element) != "AsIs"]
-            df[[i]] <- .removeAsIs(element)
-        }
-    }
-    df
-}
-
-#' @export
-.df2json <- function (df) {
-    df <- .removeAsIs(df)
-    jsonlite::toJSON(df, dataframe = "rows", pretty = 1)
-}
 
 #' @export
 setMethod("compileTrackData", signature = "data.frame",
