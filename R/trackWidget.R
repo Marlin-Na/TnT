5040d8ebf7ac7ae3292f0556d3bae50f2ddaeeb7
Marlin- <marlin-@gmx.cn>
2017-09-09 01:03:03 +0800

    Add shiny bindings

--- R/trackWidget.R
+++ R/trackWidget.R
@@ -1,6 +1,25 @@
 
 
 
+#' Convert a TnTBoard to htmlwidget
+#' 
+#' This function is only provided for users who are familiar with the concept of
+#' \link[htmlwidgets]{htmlwidgets-package}. It explicitly convets a TnTBoard or
+#' TnTGenome object to a htmlwidget object. You do not need it in most cases.
+#'
+#' @param tntdef A TnTBoard/TnTGenome object or a htmlwidget object. If it is
+#'     a htmlwidget object, the function will return it as is.
+#' @param elementId An id for the htmlwidget (random by default).
+#' 
+#' @return
+#' The function returns a htmlwidget object.
+#' 
+#' @export
+#' @examples
+#' b <- TnTBoard(BlockTrack(GRanges("chr12", IRanges(1, 123))))
+#' widget <- trackWidget(b)
+#' class(widget)
+#' identical(widget, trackWidget(widget))
 trackWidget <- function (tntdef, elementId = NULL) {
     # Determine the class of tntdef
     if (inherits(tntdef, "TnTBoard"))
@@ -9,6 +28,9 @@ trackWidget <- function (tntdef, elementId = NULL) {
         tntdef <- asJS(tntdef)
     if (inherits(tntdef, "JavaScript"))
         tntdef <- as.character(tntdef)
+    
+    if (inherits(tntdef, "htmlwidget"))
+        return(tntdef)
     
     stopifnot(is.character(tntdef))
     
@@ -77,61 +99,55 @@ if (interactive()) local({
 # TODO: provide shiny bindings
 
 
-# #' Shiny bindings for TnT
-# #'
-# #' Output and render functions for using TnT within Shiny
-# #' applications and interactive Rmd documents.
-# #'
-# #' @param outputId output variable to read from
-# #' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-# #'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-# #'   string and have \code{'px'} appended.
-# #' @param expr An expression that generates a TnT
-# #' @param env The environment in which to evaluate \code{expr}.
-# #' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-# #'   is useful if you want to save an expression in a variable.
-# #'
-# #' @name TnT-shiny
-# #'
-# #' @export
-# #' @examples
-# #' if (interactive() && require(shiny)) {
-# #'     ui <- fluidPage(
-# #'         fluidRow(TnTOutput("out"))
-# #'     )
-# #'     server <- function (input, output) {
-# #'         output$out <- renderTnT({
-# #'             TnT("
-# #'               tnt.board.genome()
-# #'                 .from(0)
-# #'                 .to(500)
-# #'                 .min(0)
-# #'                 .max(1000)
-# #'                 .add_track(tnt.board.track()
-# #'                            .height(20)
-# #'                            .color('white')
-# #'                            .display(tnt.board.track.feature.axis()))
-# #'                 .add_track(tnt.board.track()
-# #'                            .height(30)
-# #'                            .color('#FFCFDD')
-# #'                            .data(tnt.board.track.data.sync()
-# #'                                  .retriever(function() {return [{start : 200, end : 350}]}))
-# #'                            .display(tnt.board.track.feature.block()
-# #'                                     .color('blue')
-# #'                                     .index(function (d) {return d.start})))
-# #'             ")
-# #'         })
-# #'     }
-# #'     shinyApp(ui = ui, server = server)
-# #' }
-# TnTOutput <- function(outputId, width = '100%', height = '400px'){
-#     htmlwidgets::shinyWidgetOutput(outputId, 'TnT', width, height, package = 'TnT')
-# }
-# 
-# #' @rdname TnT-shiny
-# #' @export
-# renderTnT <- function(expr, env = parent.frame(), quoted = FALSE) {
-#     if (!quoted) { expr <- substitute(expr) } # force quoted
-#     htmlwidgets::shinyRenderWidget(expr, TnTOutput, env, quoted = TRUE)
-# }
+#' Shiny bindings for TnT
+#'
+#' Output and render functions for using TnT within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a TnTBoard/TnTGenome object.
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name TnT-shiny
+#' @export
+#' @examples
+#' if (interactive() && require(shiny)) {
+#'     ui <- fluidPage(fluidRow(
+#'         column(width = 2, {
+#'             "A Simple Example Here"
+#'         }),
+#'         column(width = 10, {
+#'             TnTOutput("out")
+#'         })
+#'     ))
+#'     server <- function (input, output) {
+#'         re.btrack <- reactive({
+#'             gr <- GRanges("chr12", IRanges(100, 1000))
+#'             BlockTrack(gr)
+#'         })
+#'         output$out <- renderTnT({
+#'             TnTBoard(re.btrack())
+#'         })
+#'     }
+#'     shinyApp(ui = ui, server = server)
+#' }
+TnTOutput <- function (outputId, width = '100%', height = 'auto') {
+    htmlwidgets::shinyWidgetOutput(outputId, 'trackWidget', width, height, package = "TnT")
+}
+
+#' @rdname TnT-shiny
+#' @export
+renderTnT <- function (expr, env = parent.frame(), quoted = FALSE) {
+    if (!quoted) expr <- substitute(expr) # force quoted
+    
+    # A little bit hack, maybe there are better ways
+    expr <- bquote(TnT::trackWidget(.(expr)))
+    
+    htmlwidgets::shinyRenderWidget(expr, TnTOutput, env, quoted = TRUE)
+}
 
