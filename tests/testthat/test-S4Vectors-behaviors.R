5d654eae49099033d9514ca6f806ef3ae582e117
Marlin- <marlin-@gmx.cn>
2018-07-27 05:04:24 +0800

    Remove out-date tests

--- tests/testthat/test-S4Vectors-behaviors.R
+++ tests/testthat/test-S4Vectors-behaviors.R
@@ -12,37 +12,38 @@ suppressPackageStartupMessages({
     library(S4Vectors)
 })
 
-
-test_that("as.data.frame(DF) with nested df will have the nested df wrapped in AsIs class", {
-    DF <- DataFrame(x = c(1:5))
-    DF$nested <- data.frame(
-        nested_a = runif(5),
-        nested_b = paste0("aa", 1:5)
-    )
-    converted.df <- as.data.frame(DF)
-    expect_identical(unclass(converted.df$nested), unclass(DF$nested))
-    
-    # Error is associated with printing method of the converted data frame
-    expect_error(print(converted.df), "dims .* do not match the length of object")
-    # Reason is that the column of nested data frame has an AsIs class
-    expect_identical(class(converted.df$x), "integer")
-    expect_identical(class(converted.df$nested), c("AsIs", "data.frame"))
-    # Unwrap the AsIs class
-    cl <- class(converted.df$nested)
-    class(converted.df$nested) <- cl[cl != "AsIs"]
-    # No error now
-    capture_output(print(converted.df))
-})
-
-test_that("DF with nested DF can not be converted to data.frame", {
-    DF <- DataFrame(x = c(1:5))
-    DF$nested <- DataFrame(
-        nested_a = runif(5),
-        nested_b = paste0("aa", 1:5)
-    )
-    
-    expect_error(as.data.frame(DF))
-})
+# Behaviors of S4Vectors have been changed, nested DataFrame will be flatten in as.data.frame
+# 
+# test_that("as.data.frame(DF) with nested df will have the nested df wrapped in AsIs class", {
+#     DF <- DataFrame(x = c(1:5))
+#     DF$nested <- data.frame(
+#         nested_a = runif(5),
+#         nested_b = paste0("aa", 1:5)
+#     )
+#     converted.df <- as.data.frame(DF)
+#     expect_identical(unclass(converted.df$nested), unclass(DF$nested))
+#     
+#     # Error is associated with printing method of the converted data frame
+#     expect_error(print(converted.df), "dims .* do not match the length of object")
+#     # Reason is that the column of nested data frame has an AsIs class
+#     expect_identical(class(converted.df$x), "integer")
+#     expect_identical(class(converted.df$nested), c("AsIs", "data.frame"))
+#     # Unwrap the AsIs class
+#     cl <- class(converted.df$nested)
+#     class(converted.df$nested) <- cl[cl != "AsIs"]
+#     # No error now
+#     capture_output(print(converted.df))
+# })
+# 
+# test_that("DF with nested DF can not be converted to data.frame", {
+#     DF <- DataFrame(x = c(1:5))
+#     DF$nested <- DataFrame(
+#         nested_a = runif(5),
+#         nested_b = paste0("aa", 1:5)
+#     )
+#     
+#     expect_error(as.data.frame(DF))
+# })
 
 # Has been fixed in S4Vectors version 0.15.10
 #
